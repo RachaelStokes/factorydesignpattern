733caa594f6571ad9c5cba9d4ccd622fa17e9b4f
Rachael Stokes <stokesrachaele@gmail.com>
2021-02-05 11:25:52 -0500

    javadoc in kidsbike class

--- factorydesignpattern/KidsBike.java
+++ factorydesignpattern/KidsBike.java
@@ -1,11 +1,17 @@
 package factorydesignpattern;
-
+/**
+ * child class of bike that sets the attributes according to a kid's bike
+ * @author Rachael
+ */
 public class KidsBike extends Bike{
+    /**
+     * constructor method to set all values of the kid's bike
+     */
     public KidsBike() {
-        name = "KidsBike";
-        price = 80.99;
-        numOfWheels = 2;
-        hasPedals = true;
-        hasTrainingWheels = true;
+        name = "KidsBike"; //sets the name of the bike to "kidsbike"
+        price = 80.99; //sets the price of the bike to 80.99
+        numOfWheels = 2; //sets the number of wheels to 2
+        hasPedals = true; //sets the pedals to true
+        hasTrainingWheels = true; //sets the training wheels to true
     }
 }
