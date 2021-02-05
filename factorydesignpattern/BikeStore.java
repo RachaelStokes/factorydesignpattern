87443e9a7f4a63ef11dbee8d7b5750f248f802c5
Rachael Stokes <stokesrachaele@gmail.com>
2021-02-05 11:34:13 -0500

    added javadoc to bikestore class

--- factorydesignpattern/BikeStore.java
+++ factorydesignpattern/BikeStore.java
@@ -1,9 +1,25 @@
 package factorydesignpattern;
 
+/**
+ * class bike store creates and orders the bikes 
+ * @author Rachael 
+ */
 public class BikeStore {
+
+    /**
+     * public method returns the private createBike method using parameter type
+     * @param type the string type of the bike
+     * @return Bike the created bike from the string
+     */
     public Bike orderBike(String type) {
         return createBike(type);
     }
+
+    /**
+     * private method returns a bike created based on what type it is and prints the price
+     * @param type the string type of the bike
+     * @return Bike the bike created
+     */
     private Bike createBike(String type) {
         Bike bike;
         if (type.equals("tricycle")) {
