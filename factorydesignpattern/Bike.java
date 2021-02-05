aadcf503f547b0f8a26d85ea760a13ca4d02ef1d
Rachael Stokes <stokesrachaele@gmail.com>
2021-02-05 10:54:14 -0500

    edited bike class

--- factorydesignpattern/Bike.java
+++ factorydesignpattern/Bike.java
@@ -8,19 +8,28 @@ public abstract class Bike {
     protected boolean hasTrainingWheels;
 
     public void createBike() {
-
+        createFrame();
+        addWheels();
+        addPedals();
     }
 
     private void createFrame() {
-
+        System.out.println("Assembling " + name + " frame");
     }
 
     private void addWheels() {
-
+        if(numOfWheels != 0) {
+            System.out.println("Adding " + numOfWheels + "wheel(s)");
+            if(hasTrainingWheels) {
+                System.out.println("Adding Training Wheels");
+            }
+        } 
     }
 
     private void addPedals() {
-
+        if(hasPedals) {
+            System.out.println("Adding Pedals");
+        }
     }
 
     public double getPrice() {
