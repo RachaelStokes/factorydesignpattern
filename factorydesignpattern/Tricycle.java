afecf68f4f15ee8ad60ed35b66368103d7d71568
Rachael Stokes <stokesrachaele@gmail.com>
2021-02-05 11:29:03 -0500

    javadoc in tricycle class

--- factorydesignpattern/Tricycle.java
+++ factorydesignpattern/Tricycle.java
@@ -1,11 +1,18 @@
 package factorydesignpattern;
-
+/**
+ * child class of bike that sets the attributes according to a tricycle
+ * @author Rachael
+ */
 public class Tricycle extends Bike {
+
+    /**
+     * constructor method to set all values of the tricycle
+     */
     public Tricycle() {
-        name = "Tricycle";
-        price = 54.95;
-        numOfWheels = 3;
-        hasPedals = true;
-        hasTrainingWheels = false;
+        name = "Tricycle"; //sets the name of the bike to tricycle
+        price = 54.95; //sets the price to 54.95
+        numOfWheels = 3; //sets the number of wheels to 3
+        hasPedals = true; //sets the pedals to true
+        hasTrainingWheels = false; //sets the training wheels to false
     }
 }
