7fe06f1c2c402d8a3bc8802eae0a6535710decb2
Rachael Stokes <stokesrachaele@gmail.com>
2021-02-05 10:59:27 -0500

    added tricycle class

---
+++ factorydesignpattern/Tricycle.java
@@ -0,0 +1,11 @@
+package factorydesignpattern;
+
+public class Tricycle extends Bike {
+    public Tricycle() {
+        name = "Tricycle";
+        price = 54.95;
+        numOfWheels = 3;
+        hasPedals = true;
+        hasTrainingWheels = true;
+    }
+}
